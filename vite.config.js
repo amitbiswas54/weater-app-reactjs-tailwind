8108ffec77891603885b531ba40651d87861903e
Amit Biswas <amit.biswas54@gmail.com>
2026-02-04 12:25:42 +0530

    add base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/weater-app-reactjs-tailwind/',
   plugins: [react(),tailwindcss()],
 })
